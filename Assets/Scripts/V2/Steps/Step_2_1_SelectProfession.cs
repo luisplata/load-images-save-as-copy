85e6ebfe01cd0cdba45bd8d13cc987fdd886e5de
Luis Plata <www.luisplata@gmail.com>
2024-07-11 20:54:22 -0500

    create keys to server read it

--- Assets/Scripts/V2/Steps/Step_2_1_SelectProfession.cs
+++ Assets/Scripts/V2/Steps/Step_2_1_SelectProfession.cs
@@ -9,29 +9,29 @@ namespace V2.Steps
         [SerializeField] private TMP_Dropdown professionDropdown;
         private TeaTime flow;
         private bool readyToNextStep;
-        
-        private Dictionary<string, string> professions = new Dictionary<string, string>
+
+        private Dictionary<string, string> professions = new()
         {
-            {"Grupo 1", "Convert the image into a stylish company CEO by incorporating business items and elegant restaurant"},
-            {"Grupo 2", "Convert the image into a detailed and precise financial illustration incorporate numbers and Financial items"},
-            {"Grupo 3", "Convert the image into a naturalism style incorporate agronomy trade and ecology items"},
-            {"Grupo 4", "Create an artistic collage using the image and other visuals to incorporate Logistics and international Business items"},
-            {"Grupo 5", "Convert image into professional Workplace Safety and Human Resources drawing with dynamic perspective and depth effects"},
-            {"Grupo 6", "Turn the image into a professional lawyer in opulent style incorporate justness items"},
-            {"Grupo 7", "Convert the image a teacher, like a child’s drawing incorporate education items Turn the image into a teacher in the classroom, as if it were a child's drawing, turn the image a teacher, like a child’s drawing Convert the image to a teacher in classroom"},
-            {"Grupo 8", "Convert the image into a psychology digital illustration with crisp lines apply grid art style incorporate psychology and people items"},
-            {"Grupo 9", "Convert the image into a photograph with smooth light and motion effects incorporate journalism and mass media items"},
-            {"Grupo 10", "Create an artistic collage using the image and other visuals to incorporate marketing and publicity items"},
-            {"Grupo 11", "Give the image an industrial design painting look with precise details and clean lines to incorporate industrial design elements around"},
-            {"Grupo 12", "Give the image a futuristic look with technological and abstract elements"},
-            {"Grupo 13", "Give the image a stylish and sophisticated fashion illustration look"},
-            {"Grupo 14", "Convert the image into a detailed and precise financial an mathematical illustration+to incorporate numbers and mathematical items"},
+            { "Grupo 1", "Grupo1" },
+            { "Grupo 2", "Grupo2" },
+            { "Grupo 3", "Grupo3" },
+            { "Grupo 4", "Grupo4" },
+            { "Grupo 5", "Grupo5" },
+            { "Grupo 6", "Grupo6" },
+            { "Grupo 7", "Grupo7" },
+            { "Grupo 8", "Grupo8" },
+            { "Grupo 9", "Grupo9" },
+            { "Grupo 10", "Grupo10" },
+            { "Grupo 11", "Grupo11" },
+            { "Grupo 12", "Grupo12" },
+            { "Grupo 13", "Grupo13" },
+            { "Grupo 14", "Grupo14" }
         };
 
         protected override void Start()
         {
             base.Start();
-            
+
             professionDropdown.onValueChanged.AddListener(value =>
             {
                 if (value == 0)
@@ -41,6 +41,7 @@ namespace V2.Steps
                     flow.Play();
                     return;
                 }
+
                 stepsConfig.SaveProfessionSelected(professions[professionDropdown.options[value].text]);
                 readyToNextStep = true;
             });
@@ -49,9 +50,7 @@ namespace V2.Steps
         public override void StartStep()
         {
             flow = this.tt().Pause()
-                .Add(() =>
-                {
-                }).Wait(() => readyToNextStep).Add(ShowNextButton);
+                .Add(() => { }).Wait(() => readyToNextStep).Add(ShowNextButton);
             flow.Play();
         }
     }
